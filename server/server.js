f16f486682f38a3b4eac0bd68986174d4464e0d7
Mezekr W <mezi.edu@tuta.io>
2024-10-14 21:24:36 +0200

    add port number .env file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 
-const PORT = 3000;
+const PORT = process.env.PORT_NUMBER || 3000;
 
 app.listen(PORT, () => {
 	console.log(`App listening to request on PORT ${PORT}.`);
