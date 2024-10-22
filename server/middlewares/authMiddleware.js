a69c08daa045f16fb0f11dc58e96d8380db35987
Mezekr W <mezi.edu@tuta.io>
2024-10-22 20:59:40 +0200

    change import to defaults to CommonJS modules

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
-import { jwt } from 'jsonwebtoken';
+const jwt = require('jsonwebtoken');
 
-export default (req, res, next) => {
+module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(' ')[1];
 		const decoded = jwt.verify(token, process.env.SECRET_KEY);
