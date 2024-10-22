1536f46a77c63108d3ec589d174f2ed39ccb8833
Mezekr W <mezi.edu@tuta.io>
2024-10-22 21:02:15 +0200

    add api route to get logged in user

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const authRouter = require('./controllers/authController');
+const userRouter = require('./controllers/userController');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/auth/', authRouter);
+app.use('/api/user/', userRouter);
 
 module.exports = app;
---
+++ server/controllers/userController.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const authMiddleware = require('./../middlewares/authMiddleware');
+const User = require('./../models/user');
+
+router.get('/get-logged-user', authMiddleware, async (req, res) => {
+	try {
+		const user = await User.findOne({ _id: req.body.userId });
+		res.send({
+			message: 'user details fetched successfully',
+			success: true,
+			data: user,
+		});
+	} catch (error) {
+		res.send({
+			message: error.message,
+			status: false,
+		});
+	}
+});
+module.exports = router;
