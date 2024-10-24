85b9b96ccc45fb5270335f2837dbc2f7d735602b
Mezekr W <mezi.edu@tuta.io>
2024-10-24 19:54:01 +0200

    add api endpoint to get all users from DB

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const authMiddleware = require('./../middlewares/authMiddleware');
 const User = require('./../models/user');
 
-router.get('/get-logged-user', authMiddleware, async (req, res) => {
+router.get('/', authMiddleware, async (req, res) => {
 	try {
 		const user = await User.findOne({ _id: req.body.userId });
 		res.send({
@@ -17,4 +17,21 @@ router.get('/get-logged-user', authMiddleware, async (req, res) => {
 		});
 	}
 });
+
+router.get('/get-all-users', authMiddleware, async (req, res) => {
+	try {
+		const users = await User.find({ _id: { $ne: req.body.userId } });
+		res.send({
+			message: 'All user details fetched successfully',
+			success: true,
+			data: users,
+		});
+	} catch (error) {
+		res.send({
+			message: error.message,
+			status: false,
+		});
+	}
+});
+
 module.exports = router;
