421c794402455e185da5b5f1fa224fda57398555
Mezekr W <mezi.edu@tuta.io>
2024-11-26 13:00:53 +0100

    fig typo in code bug

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
 const authMiddleware = require('./../middlewares/authMiddleware');
-const chat = require('./../models/chat');
+const Chat = require('./../models/chat');
 
 router.post('/create-new-chat', authMiddleware, async (req, res) => {
 	try {
-		const chat = new chat(req.body);
+		const chat = new Chat(req.body);
 		const savedChat = await chat.save();
-		res.stauts(201).send({
+		res.status(201).send({
 			message: 'Chat created successfully',
 			success: true,
 			data: savedChat,
 		});
 	} catch (error) {
-		res.stats(400).send({
+		res.status(400).send({
 			message: error.message,
 			success: false,
 		});
